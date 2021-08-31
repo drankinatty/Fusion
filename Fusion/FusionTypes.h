e59d7ef3016036539a4642367cf804b69ce31564
David C. Rankin <drankinatty@gmail.com>
2021-08-31 04:19:58 -0500

    Fix strict-aliasing violation (type punned pointers) in FusionFastInverseSqrt() using union between float and int32_t.

--- Fusion/FusionTypes.h
+++ Fusion/FusionTypes.h
@@ -144,9 +144,9 @@ static inline __attribute__((always_inline)) float FusionRadiansToDegrees(const 
 static inline __attribute__((always_inline)) float FusionFastInverseSqrt(const float x) {
     float halfx = 0.5f * x;
     float y = x;
-    int32_t i = *(int32_t*) & y;
-    i = 0x5f3759df - (i >> 1);
-    y = *(float*) &i;
+    union { float f; int32_t i; } fi = { .f = y };
+    fi.i = 0x5f3759df - (fi.i >> 1);
+    y = fi.f;
     y = y * (1.5f - (halfx * y * y));
     return y;
 }
